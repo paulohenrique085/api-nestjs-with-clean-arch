955ec66b6e533b9397f7cb7f06ebd79bb5c3c18f
paulohenrique085 <paulohnq55@outlook.com>
2024-01-17 22:30:13 -0300

    feat: adjusting file that was automatically created by nest to meet the needs of my core

---
+++ src/location/location.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { CreateLocationDto } from './dto/create-location.dto';
+import { RegisterLocation } from 'src/@core/application/@usecases/RegisterLocation';
+import { GetAllLocations } from 'src/@core/application/@usecases/GetAllLocations';
+
+@Controller('location')
+export class LocationController {
+  constructor(private registerLocation: RegisterLocation, private getAllLocations: GetAllLocations) { }
+
+  @Post('/register')
+  create(@Body() createLocationDto: CreateLocationDto) {
+    return this.registerLocation.execute(createLocationDto);
+  }
+
+  @Get("/locations")
+  findAll() {
+    return this.getAllLocations.execute();
+  }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.locationService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateLocationDto: UpdateLocationDto) {
+  //   return this.locationService.update(+id, updateLocationDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.locationService.remove(+id);
+  // }
+}
