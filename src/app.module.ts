9736a912a899fdef0327cedb9957a4184b0b8b79
paulohenrique085 <paulohnq55@outlook.com>
2024-01-17 22:28:06 -0300

    feat: creation default nest API REST

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LocationModule } from './location/location.module';
 
 @Module({
-  imports: [],
+  imports: [LocationModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
