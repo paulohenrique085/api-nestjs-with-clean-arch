f65140873a883426cf64c2f57518539b84ee0f14
Paulo Henrique <paulo@MacBook-Pro-de-Ivo.local>
2024-01-28 14:14:19 -0300

    feat: create DTO validations

--- src/location/dto/create-location.dto.ts
+++ src/location/dto/create-location.dto.ts
@@ -1,4 +1,10 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateLocationDto {
+  @IsString()
+  @IsNotEmpty()
   latitude: string;
+  @IsString()
+  @IsNotEmpty()
   longitude: string;
 }
